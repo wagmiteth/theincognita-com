e80ad2c59346993cdc7a4ffdd2400db73f046923
wagmiteth <hello@wagmi.se>
2023-06-10 08:14:26 +0200

    updated bases

--- src/components/Bases.js
+++ src/components/Bases.js
@@ -12,47 +12,46 @@ const Bases = ({ record }) => {
   let modifiedBase = baseArray.map((base) => {
     base = base.trim();
     if (base.includes("Africa")) {
-      return <span id='emoji'>🌍</span>;
+      return <span className='emoji'>🌍</span>;
     } else if (base.includes("Global")) {
-      return <span id='emoji'>🛬</span>;
+      return <span className='emoji'>🛬</span>;
     } else if (base.includes("North America")) {
-      return <span id='emoji'>🌎</span>;
+      return <span className='emoji'>🌎</span>;
     } else if (base.includes("Latin America")) {
-      return <span id='emoji'>💃</span>;
+      return <span className='emoji'>💃</span>;
     } else if (base.includes("Europe")) {
-      return <span id='emoji'>🇪🇺</span>;
+      return <span className='emoji'>🇪🇺</span>;
     } else if (base.includes("Asia")) {
-      return <span id='emoji'>🀄 </span>;
+      return <span className='emoji'>🀄 </span>;
     } else if (base.includes("US")) {
-      return <span id='emoji'>🇺🇸</span>;
+      return <span className='emoji'>🇺🇸</span>;
     } else if (base.includes("Honduras")) {
-      return <span id='emoji'>🇭🇳</span>;
+      return <span className='emoji'>🇭🇳</span>;
     } else if (base.includes("Palau")) {
-      return <span id='emoji'>🇵🇼</span>;
+      return <span className='emoji'>🇵🇼</span>;
     } else if (base.includes("Oceania")) {
-      return <span id='emoji'>🏄🏼‍♂️</span>;
+      return <span className='emoji'>🏄🏼‍♂️</span>;
     } else if (base.includes("Vanuatu")) {
-      return <span id='emoji'>🇻🇺</span>;
+      return <span className='emoji'>🇻🇺</span>;
     } else if (base.includes("Singapore")) {
-      return <span id='emoji'>🇸🇬</span>;
+      return <span className='emoji'>🇸🇬</span>;
     } else if (base.includes("Nigeria")) {
-      return <span id='emoji'>🇳🇬</span>;
+      return <span className='emoji'>🇳🇬</span>;
     } else {
       return base;
     }
   });
-  
-  
 
   if (modifiedBase) {
     return (
-       <div className="base" style={{ display: "flex", flexDirection: "row" }}>
-  {modifiedBase.map((base, index) => (
-    <div key={index} style={{ margin: "2px" }}>{base}</div>
-  ))}
-  <ApplicationButton record={record} id="JoinExpanded" />
-</div>
-
+      <div className="base" style={{ display: "flex", flexDirection: "row", justifyContent: "space-between" }}>
+        <div style={{ display: "flex", flexDirection: "row" }}>
+          {modifiedBase.map((base, index) => (
+            <div key={index} style={{ margin: "2px" }}>{base}</div>
+          ))}
+        </div>
+        <ApplicationButton record={record} id="JoinExpanded" />
+      </div>
     );
   } else {
     return null;
